6eb1b9284d5023ce9e68a1552eaf2875449d1014
Hasibul Sakib <hasibulsakib268@gmail.com>
2026-05-05 08:55:27 +0600

    Add files via upload

---
+++ Div-4/Div4-1090-A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,y;
+    cin >> t;
+    while(t--){
+        int x;
+        cin>> x;
+        y=x+1;
+        int z=min(x,y);
+        cout << z << endl;
+    }
+}
